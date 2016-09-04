b39b8058d7f0fa700c316f4b128a2f8ecfbb4527
Quang Ngô <ngoquang2708@gmail.com>
2016-09-04 11:49:40 +0700

    init: move init.swap.rc to system/etc/init

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -51,7 +51,6 @@ ROOTDIR_FILES := \
 	device/samsung/vivalto3gvn/rootdir/init.vivalto3gvn.rc \
 	device/samsung/vivalto3gvn/rootdir/init.vivalto3gvn_base.rc \
 	device/samsung/vivalto3gvn/rootdir/init.wifi.rc \
-	device/samsung/vivalto3gvn/rootdir/init.swap.rc \
 	device/samsung/vivalto3gvn/rootdir/ueventd.scx15.rc \
 	device/samsung/vivalto3gvn/rootdir/fstab.scx15 \
 	device/samsung/vivalto3gvn/rootdir/fstab.swap \
@@ -78,6 +77,7 @@ SYSTEM_INIT_RC_FILES := \
 	device/samsung/vivalto3gvn/system/etc/init/rild.rc \
 	device/samsung/vivalto3gvn/system/etc/init/set_mac.rc \
 	device/samsung/vivalto3gvn/system/etc/init/smd_symlink.rc \
+	device/samsung/vivalto3gvn/system/etc/init/swap.rc \
 	device/samsung/vivalto3gvn/system/etc/init/wpa_supplicant.rc \
 
 PRODUCT_COPY_FILES += \
