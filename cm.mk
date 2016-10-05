e410aed59a4afee5116410be349fa95c69e7d54a
Quang Ngô <ngoquang2708@gmail.com>
2016-10-05 17:47:41 +0700

    Revert "staging/cm-14.0 missed Browser package"
    This reverts commit aeaaeb9c207bda998d1dde3dec986785949f85ed.
    
    See: http://review.cyanogenmod.org/#/c/164784

--- cm.mk
+++ cm.mk
@@ -31,9 +31,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Override build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-PRODUCT_PACKAGES += \
-	Browser \
-
 # Device identifier
 PRODUCT_DEVICE := vivalto3gvn
 PRODUCT_NAME := cm_vivalto3gvn
