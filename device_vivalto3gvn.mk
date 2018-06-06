7ed503430ed97c54493e049f2d1372fe7cd26f27
Quang Ngô <ngoquang2708@gmail.com>
2018-06-06 19:17:18 +0700

    Build sprd stagefright codecs

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -159,17 +159,17 @@ PRODUCT_PACKAGES += \
 	libbt-vendor \
 
 # Codecs
-#PRODUCT_PACKAGES += \
-#	libstagefrighthw \
-#	libstagefright_sprd_soft_mpeg4dec \
-#	libstagefright_sprd_soft_h264dec \
-#	libstagefright_sprd_mpeg4dec \
-#	libstagefright_sprd_mpeg4enc \
-#	libstagefright_sprd_h264dec \
-#	libstagefright_sprd_h264enc \
-#	libstagefright_sprd_vpxdec \
-#	libstagefright_sprd_aacdec \
-#	libstagefright_sprd_mp3dec \
+PRODUCT_PACKAGES += \
+	libstagefrighthw \
+	libstagefright_sprd_soft_mpeg4dec \
+	libstagefright_sprd_soft_h264dec \
+	libstagefright_sprd_mpeg4dec \
+	libstagefright_sprd_mpeg4enc \
+	libstagefright_sprd_h264dec \
+	libstagefright_sprd_h264enc \
+	libstagefright_sprd_vpxdec \
+	libstagefright_sprd_aacdec \
+	libstagefright_sprd_mp3dec \
 
 # Shim
 PRODUCT_PACKAGES += \
