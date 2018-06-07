d0a55b14c5905a1bbba0842f9a0f0a52f3f78890
Quang Ngô <ngoquang2708@gmail.com>
2018-06-07 22:07:18 +0700

    Add headset-keyboard.kl

--- device_vivalto3gvn.mk
+++ device_vivalto3gvn.mk
@@ -81,7 +81,8 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 files := \
 	$(LOCAL_PATH)/system/vendor/usr/keylayout/ist30xx_ts_input.kl \
-	$(LOCAL_PATH)/system/vendor/usr/keylayout/sci-keypad.kl
+	$(LOCAL_PATH)/system/vendor/usr/keylayout/sci-keypad.kl \
+	$(LOCAL_PATH)/system/vendor/usr/keylayout/headset-keyboard.kl \
 
 PRODUCT_COPY_FILES += \
 	$(foreach f,$(files),$(f):$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/$(notdir $(f)))
