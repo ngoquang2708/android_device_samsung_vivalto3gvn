339a9e104d6c6ce859261a44106f75b252dd79e9
Quang Ngô <ngoquang2708@gmail.com>
2018-07-21 23:55:46 +0700

    Move packages inclusion to a separated Makefile

--- vivalto3gvn.mk
+++ vivalto3gvn.mk
@@ -144,54 +144,6 @@ files := \
 PRODUCT_COPY_FILES += \
 	$(foreach f,$(files),$(f):$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/$(notdir $(f)))
 
-# HALs
-PRODUCT_PACKAGES += \
-	audio.primary.scx15 \
-	libaudio-resampler \
-	bluetooth.default \
-	memtrack.scx15 \
-	gralloc.scx15 \
-	hwcomposer.scx15 \
-	sprd_gsp.scx15 \
-	libdither \
-	lights.scx15 \
-	libion_sprd \
-	libmemoryheapion_sprd \
-	libbt-vendor \
-	sensors.scx15 \
-
-# RIL
-PRODUCT_PACKAGES += \
-	modemd \
-	nvitemd \
-	refnotify \
-	rild \
-	libsecril-client \
-	libatchannel \
-
-# Codecs
-PRODUCT_PACKAGES += \
-	libstagefrighthw \
-	libstagefright_sprd_soft_mpeg4dec \
-	libstagefright_sprd_soft_h264dec \
-	libstagefright_sprd_mpeg4dec \
-	libstagefright_sprd_mpeg4enc \
-	libstagefright_sprd_h264dec \
-	libstagefright_sprd_h264enc \
-	libstagefright_sprd_vpxdec \
-	libstagefright_sprd_aacdec \
-	libstagefright_sprd_mp3dec \
-
-# Shim
-PRODUCT_PACKAGES += \
-	libsecril-shim \
-	libril_shim \
-	libgps_shim \
-	libstagefright_shim \
-	libphoneserver_shim \
-
-PRODUCT_PACKAGES += \
-	Snap \
-
+$(call inherit-product, $(DEVICE_PATH)/packages.mk)
 $(call inherit-product, $(DEVICE_PATH)/prop.mk)
 $(call inherit-product, $(DEVICE_PATH)/treble.mk)
